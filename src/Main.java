01dadeede5a9d51bde22570b8c91c32a2615d47a
taigoti <taigoti@proton.me>
2026-06-10 00:01:03 -0300

    implements first methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     static void main(String[] args) {
+        Movie meuFilme = new Movie(
+                "Matrix", "Ficção", "Wachowski",
+                1999, 137, 4.8);
 
+        meuFilme.getInfos();
     }
 }
--- src/Movies.java
+++
@@ -1,7 +0,0 @@
-public class Movies {
-    String name;
-    String genre;
-    String director;
-    int year;
-    int duration;
-}
